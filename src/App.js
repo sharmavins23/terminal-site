16e60bb7821310b941b2a0ef24aaf50d3bbb6ac6
sharmavins23 <sharmavins23@gmail.com>
2022-05-02 16:33:18 -0400

    added basic construction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { ReactTerminal } from "react-terminal";
 
 function App() {
+    const welcomeMessage = (
+        <>
+            Welcome to the interactive <b>VINS.exe</b> command line.
+            <br />
+            Please type <b>help</b> to see the available commands.
+            <br />
+            <br />
+        </>
+    );
+
+    const errorMessage = (
+        <>
+            <b style={{ color: "red" }}>
+                FATAL ERR: The term is not recognized as the name of a command, function, script file, or operable program.
+            </b>
+            <br />
+            Check the spelling of the command, or type <b>help</b> to see the available commands.
+            <br />
+        </>
+    );
+
+    const commands = {
+        whoami: "You are Vins Sharma",
+        hello: "Hello World",
+    }
+
     return (
-        <div className="App">
-            Fuck this shit
-        </div>
+        <>
+            <div style={{
+                // Force set the width and height to match the parent
+                height: "98vh",
+                // Set background color as black
+                backgroundColor: "black"
+            }}>
+                <ReactTerminal
+                    welcomeMessage={welcomeMessage}
+                    prompt="$"
+                    commands={commands}
+                    errorMessage={errorMessage}
+                    enableInput={true}
+                    showControlBar={true}
+                    showControlButtons={false}
+                    theme="material-dark"
+                />
+            </div>
+        </>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { TerminalContextProvider } from "react-terminal";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <TerminalContextProvider>
+            <App />
+        </TerminalContextProvider>
     </React.StrictMode>
 );
